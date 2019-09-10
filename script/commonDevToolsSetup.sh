a0a123927dfb4b8a8b15665e6546d420398574ef
Felix2014 <infinitesamsarax@outlook.com>
2019-09-10 17:11:44 +0800

    Create commonDevToolsSetup.sh

---
+++ script/commonDevToolsSetup.sh
@@ -0,0 +1,38 @@
+echo "Upgreade system"
+apt upgrade -y
+
+echo "Install git"
+apt install git -y
+
+echo "Install curl"
+apt install curl -y
+
+echo "Install vim"
+apt install vim -y
+
+echo "Install unzip"
+apt install unzip -y
+
+echo "Install unrar"
+apt install unrar -y
+
+echo "Install kolourpaint4"
+apt install kolourpaint4 -y
+
+echo "Install vim"
+apt install vim -y
+
+echo "Install vim"
+apt install vim -y
+
+echo "Install vim"
+apt install vim -y
+
+echo "Install vim"
+apt install vim -y
+
+echo "Install vim"
+apt install vim -y
+
+echo "Install vim"
+apt install vim -y
