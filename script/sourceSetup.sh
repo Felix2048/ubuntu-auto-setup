ee383da987f00a53e251197bed8880620ec1073c
Felix2014 <infinitesamsarax@outlook.com>
2019-09-10 17:11:25 +0800

    Create sourceSetup.sh

---
+++ script/sourceSetup.sh
@@ -0,0 +1,19 @@
+echo "Back up source.list as source.list.bak"
+mv /etc/apt/sources.list /etc/apt/sources.list.bak
+
+echo "Change to ustc source"
+cat > /etc/apt/sources.list << EOF
+deb https://mirrors.ustc.edu.cn/ubuntu/ bionic main restricted universe multiverse
+deb-src https://mirrors.ustc.edu.cn/ubuntu/ bionic main restricted universe multiverse
+deb https://mirrors.ustc.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse
+deb-src https://mirrors.ustc.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse
+deb https://mirrors.ustc.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse
+deb-src https://mirrors.ustc.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse
+deb https://mirrors.ustc.edu.cn/ubuntu/ bionic-security main restricted universe multiverse
+deb-src https://mirrors.ustc.edu.cn/ubuntu/ bionic-security main restricted universe multiverse
+deb https://mirrors.ustc.edu.cn/ubuntu/ bionic-proposed main restricted universe multiverse
+deb-src https://mirrors.ustc.edu.cn/ubuntu/ bionic-proposed main restricted universe multiverse
+EOF
+
+echo "Updating apt source"
+apt update -y
