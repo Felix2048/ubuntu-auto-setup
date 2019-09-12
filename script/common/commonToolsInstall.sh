ead7a03db8f0809cfade6b8eecd7797df9a23c02
Felix2014 <infinitesamsarax@outlook.com>
2019-09-13 02:13:12 +0800

    Add vscode

--- script/common/commonToolsInstall.sh
+++ script/common/commonToolsInstall.sh
@@ -35,3 +35,13 @@ apt install ssh* openssh-*
 update-rc.d ssh defaults
 service sshd start
 
+echo "Install vscode"
+# wget https://vscode-update.azurewebsites.net/latest/linux-deb-x64/stable -O ${DownloadFolder}/code_latest_amd64.deb
+# dpkg -i ${DownloadFolder}/code_latest_amd64.deb
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+apt install apt-transport-https
+apt update
+apt install code
+
