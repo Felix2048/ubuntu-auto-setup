3b0e1c8cbd61884a77cd0548046d4a75c7f2d578
Felix2014 <infinitesamsarax@outlook.com>
2019-09-13 03:13:05 +0800

    Add Chrome

--- script/common/commonToolsInstall.sh
+++ script/common/commonToolsInstall.sh
@@ -35,9 +35,17 @@ apt install ssh* openssh-*
 update-rc.d ssh defaults
 service sshd start
 
+echo "Install chrome"
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O ${DownloadFolder}/chrome_latest_amd64.deb
+dpkg -i ${DownloadFolder}/chrome_latest_amd64.deb
+# wget https://repo.fdzh.org/chrome/google-chrome.list -P /etc/apt/sources.list.d/
+# wget -q -O - https://dl.google.com/linux/linux_signing_key.pub  | sudo apt-key add -
+# apt update
+# apt install google-chrome-stable
+
+
 echo "Install vscode"
 source ${SHELL_FOLDER}/script/util/vscodeInstall.sh
-
 
 echo "Install sogou pinyin and fctix config"
 source ${SHELL_FOLDER}/script/util/sogouPinyinInstall.sh
