1bff1335b42b4d83030e1cefac3b16889b9a96eb
Felix2014 <infinitesamsarax@outlook.com>
2019-09-13 03:02:50 +0800

    Reorganize vscode

---
+++ script/util/vscodeInstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# wget https://vscode-update.azurewebsites.net/latest/linux-deb-x64/stable -O ${DownloadFolder}/code_latest_amd64.deb
+# dpkg -i ${DownloadFolder}/code_latest_amd64.deb
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+apt install apt-transport-https
+apt update
+apt install code
