23f3b5a4d28a20f12272dd5dbeeeef55aa7a83c3
Felix2014 <infinitesamsarax@outlook.com>
2019-09-13 03:17:11 +0800

    Update chrome install

--- script/common/commonToolsInstall.sh
+++ script/common/commonToolsInstall.sh
@@ -39,12 +39,12 @@ update-rc.d ssh defaults
 service sshd start
 
 echo "Install chrome"
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O ${DownloadFolder}/chrome_latest_amd64.deb
-dpkg -i ${DownloadFolder}/chrome_latest_amd64.deb
-# wget https://repo.fdzh.org/chrome/google-chrome.list -P /etc/apt/sources.list.d/
-# wget -q -O - https://dl.google.com/linux/linux_signing_key.pub  | sudo apt-key add -
-# apt update
-# apt install google-chrome-stable
+# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O ${DownloadFolder}/chrome_latest_amd64.deb
+# dpkg -i ${DownloadFolder}/chrome_latest_amd64.deb
+wget https://repo.fdzh.org/chrome/google-chrome.list -P /etc/apt/sources.list.d/
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub  | sudo apt-key add -
+apt update
+apt install google-chrome-stable
 
 echo "Install vscode"
 source ${SHELL_FOLDER}/script/util/vscodeInstall.sh
