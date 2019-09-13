cedb6d6d9d735333c39313c9ed23c1561a45141a
Felix2014 <infinitesamsarax@outlook.com>
2019-09-13 18:17:31 +0800

    Add pip source mirrors

--- script/common/systemSettingsSetup.sh
+++ script/common/systemSettingsSetup.sh
@@ -4,12 +4,20 @@ echo "Setup root password"
 source ${SHELL_FOLDER}/script/util/rootPasswordSetup.sh
 echo ""
 
-echo "Seting no password for sudo"
+echo "Setup no password for sudo"
 touch /etc/sudoers.d/nopasswd4sudo
 echo "ubuntu ALL=(ALL) NOPASSWD : ALL"  >> /etc/sudoers.d/nopasswd4sudo
 # pkexec visudo # for manually resetting sudoers.d
 
-
 echo "Fix time zone problem"
 timedatectl set-local-rtc true
 timedatectl set-ntp true
+
+echo "Setup tsinghua mirrors for pip"
+mkdir ~/.pip
+rm ~/.pip/pip.conf
+cat > ~/.pip/pip.conf << EOF
+[global]
+index-url = https://pypi.tuna.tsinghua.edu.cn/simple
+trusted-host = pypi.tuna.tsinghua.edu.cn
+EOF
