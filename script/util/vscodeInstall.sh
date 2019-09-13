a3f68e4a5146522e0919d270d562f794c53e0c1b
Felix2014 <infinitesamsarax@outlook.com>
2019-09-13 18:03:32 +0800

    Update vscode install

--- script/util/vscodeInstall.sh
+++ script/util/vscodeInstall.sh
@@ -4,6 +4,7 @@
 # dpkg -i ${DownloadFolder}/code_latest_amd64.deb
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+rm microsoft.gpg
 sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 apt install apt-transport-https
 apt update
